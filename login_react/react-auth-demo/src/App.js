b6b3288f778b6b6c98b55f4da5afc8766cd7d78f
Anusha Singh <anushasingh@Anushas-MacBook-Air.local>
2023-11-13 18:06:05 +0530

    First commit login

---
+++ login_react/react-auth-demo/src/App.js
@@ -0,0 +1,14 @@
+import Register from "./Register";
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <main className="App"> 
+        <Register />
+      </main>
+    </>
+  );
+}
+
+export default App;
